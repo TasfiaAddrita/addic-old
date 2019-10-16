72c04490476b1c434398e5295e5e4ce8482b5abb
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-16 16:54:30 -0700

    added spotify auth

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
 from flask_cors import CORS
-from keys import YT_API_KEY
 import youtube_dl
+import spotipy
+import spotipy.util as util
+import keys
 
 app = Flask(__name__)
 CORS(app)
@@ -13,11 +15,13 @@ def index():
 @app.route('/receivejson', methods=["POST"])
 def get_json():
     yt_url = request.get_json()
-    print(youtube(yt_url))
-    return yt_url
+    print(yt_get_artist_and_track(yt_url))
+    print(yt_url)
+    spotify_auth()
+    return jsonify({})
 
 # https://stackoverflow.com/questions/50711867/youtube-api-retrieve-music-in-this-video-info-from-video-metadata
-def youtube(url):
+def yt_get_artist_and_track(url):
     ydl = youtube_dl.YoutubeDL({})
     with ydl:
         video = ydl.extract_info(url, download=False)
@@ -26,6 +30,14 @@ def youtube(url):
         'track': video['track'] 
     }
     return song
+
+def spotify_auth():
+    token = util.prompt_for_user_token('mentallyfreetobe@gmail.com', 'user-read-email', client_id=keys.SPOTIPY_CLIENT_ID, client_secret=keys.SPOTIPY_CLIENT_SECRET,redirect_uri=keys.SPOTIPY_REDIRECT_URI)
+    sp = spotipy.Spotify(auth=token)
+    # print(sp.me())
+    # print(token)
+    # return (token)
+    return sp
 
 if __name__ == '__main__':
     app.run(debug=True)
