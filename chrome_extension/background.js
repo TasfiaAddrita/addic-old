fb9e03f31d7af27555cadb68c80499dcf7bccf49
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-16 16:54:54 -0700

    added spotify auth

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -7,21 +7,37 @@ var background = {
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 var activeTab = tabs[0]
                 tabUrl = activeTab.url;
-                // video_id = tabUrl.split("=");
                 background.sendJSON(tabUrl);
             })
         });
     },
 
+    message: function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            // console.log(tabs[0].id)
+            chrome.tabs.sendMessage(tabs[0].id, { from: 'background' });
+        });
+    },
+
     sendJSON: function(data) {
         var xhr = new XMLHttpRequest();
-        // var url = "http://127.0.0.1:5000/receivejson?video_id=" + data
         var url = "http://127.0.0.1:5000/receivejson"
         console.log(url)
         xhr.open("POST", url, true);
         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         xhr.send(JSON.stringify(data));
+
+        // receive data
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4 && this.status == 200) {
+                receivedContent = JSON.parse(this.responseText)
+                // console.log(receivedContent);
+                // background.message();
+                
+            }
+        };
     }
 }
+
 
 background.listener()
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,27 +1,29 @@
 console.log("Chrome Extension ready to go");
 
-// var contentScript = {
-//     getSongTitleAndArtist: function(video_id) {
-//         // var song_content = title.split("-")
-//         // this.message(song_content)
-        
-//     },
+content = {
 
-//     message: function(content) {
-//         chrome.runtime.sendMessage(
-//             {
-//                 from: 'contentScript'
-//                 // content_one: content[0],
-//                 // content_two: content[1]
-//             }
-//         );
-//     }
-// }
+    addButton: function() {
+        // window.addEventListener('load', function () {
+        //     var video_title = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0];
+        //     video_title.insertAdjacentHTML("afterbegin", "<button class=\'spotify\' >Add to Spotify</button>");
+        //     chrome.runtime.sendMessage({from: 'contentScript'});
+        // })
+        console.log("content running")
+        chrome.runtime.sendMessage({from: 'contentScript'});
+    },
 
-window.addEventListener('load', function () {
-    var video_title = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0];
+    listener: function() {
+        chrome.runtime.onMessage.addListener(function (message, sender, response) {
+            if (message.from == 'background') {
+                console.log(message);
+            }
+        });
+    },
 
-    video_title.insertAdjacentHTML("afterbegin", "<button class=\'spotify\' >Add to Spotify</button>");
-    // contentScript.message()
-    chrome.runtime.sendMessage({from: 'contentScript'});
-})
+    run: function() {
+        content.addButton();
+        // content.listener();
+    }
+}
+
+content.run()
